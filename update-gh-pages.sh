2c327a75a7da824637709baea8dd44b684ee5346
mattcorey <matt.corey@gmail.com>
2014-09-06 21:22:29 -0400

    Getting' there

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://2c29e661761a9ebb22fe2afb40d3bd09fb0ce3a9@github.com/mcorey/pdfpdiff.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://2c29e661761a9ebb22fe2afb40d3bd09fb0ce3a9@github.com/mattcorey/pdfpdiff.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
