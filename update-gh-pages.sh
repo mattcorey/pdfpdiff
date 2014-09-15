45695838dfe2fab9d46b6b0ce4f87b026b2a7c1c
mattcorey <matt.corey@gmail.com>
2014-09-15 18:57:44 -0400

    Update update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  cp -Rf $HOME/build/* .
+  cp -Rf $HOME/build/reports .
 
   #add, commit and push files
   git add -f .
